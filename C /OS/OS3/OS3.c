b5075514cca42f06b65ae668e63f645b9b800a54
Iasonas Zakynthinos <zak@Iasonass-MacBook-Pro.fritz.box>
2025-04-09 10:08:29 +0300

    zajason magic fixes

--- C /OS/OS3/OS3.c
+++ C /OS/OS3/OS3.c
@@ -8,31 +8,39 @@
 #include <ctype.h>
 #include <sys/select.h>
 
+#define EXIT_INVALID_ARGS 1
+#define EXIT_MALLOC_FAILED 2
+#define EXIT_FORK_FAILED 3
+#define EXIT_WRITE_FAILED 4
+
+// Define the usage message exactly as specified.
+#define USAGE "Usage: ask3 <nChildren> [--random] [--round-robin]\n"
+
 bool args_handler(int *arg1, char *arg2, int *command)
 {
-    if (*arg1 <= 0 || strlen(arg2) == 0){ return false; }
-    
-    if (strcmp(arg2, "--random") == 0)
-    {
-        *command = 2; // Random
+    // Check that the number of children is positive and that the command (if provided) is non-empty.
+    if (*arg1 <= 0 || strlen(arg2) == 0) {
+        return false;
     }
-    else if(strcmp(arg2, "empty") == 0  || strcmp(arg2, "--round-robin") == 0)
-    {
-        *command = 1; // Round Robin
+
+    // If a third argument was provided, check that it is either "--random" or "--round-robin".
+    if (strcmp(arg2, "--random") == 0) {
+        *command = 2; // Random mode
     }
-    else
-    {
-        return false; // Invalid command
+    else if (strcmp(arg2, "--round-robin") == 0 || strcmp(arg2, "empty") == 0) {
+        *command = 1; // Round Robin mode (default)
+    }
+    else {
+        // Any other string is invalid.
+        return false;
     }
     return true;
 }
 
 int is_valid_integer(char *str)
 {
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (!isdigit(str[i]))
-        {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isdigit((unsigned char)str[i])) {
             return 0;
         }
     }
@@ -41,27 +49,36 @@ int is_valid_integer(char *str)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3 && argc !=2)
-    {
-        write(STDERR_FILENO, "Error: Missing or too many arguments.\n", 38);
-        write(STDERR_FILENO, "Example: ./OS_first.c 5 --round-robin\n", 38);
-        exit(EXIT_FAILURE);
+    // Valid argument counts: either 2 (only nChildren) or 3 (nChildren and scheduling option).
+    if (argc != 2 && argc != 3) {
+        write(STDERR_FILENO, USAGE, strlen(USAGE));
+        exit(EXIT_INVALID_ARGS);
     }
 
     int N = atoi(argv[1]);
     char *msg;
-    if (argc == 3)
-    {
+    // If no third argument is provided, use "empty" to denote the default case.
+    if (argc == 3) {
         msg = argv[2];
     }
-    else
-    {
+    else {
         msg = "empty";
     }
 
     int command = 0;
-    int ptc[N][2]; // Parent to child
-    int ctp[N][2]; // Child to parent
+    // Validate the command argument. If the third argument is not either "--round-robin" or "--random", args_handler returns false.
+    if (!args_handler(&N, msg, &command)) {
+        write(STDERR_FILENO, USAGE, strlen(USAGE));
+        exit(EXIT_INVALID_ARGS);
+    }
+
+    // Allocate pipes for communication (parent-to-child and child-to-parent).
+    int (*ptc)[2] = malloc(N * sizeof(int[2]));
+    int (*ctp)[2] = malloc(N * sizeof(int[2]));
+    if (ptc == NULL || ctp == NULL) {
+        perror("malloc failed for pipes");
+        exit(EXIT_MALLOC_FAILED);
+    }
     for (int i = 0; i < N; i++) {
         if (pipe(ptc[i]) == -1 || pipe(ctp[i]) == -1) {
             perror("pipe failed");
@@ -69,112 +86,99 @@ int main(int argc, char *argv[])
         }
     }
 
+    // Allocate an array to keep track of child process IDs.
     pid_t *child_pids = (pid_t *)malloc(N * sizeof(pid_t));
-    if (child_pids == NULL)
-    {
+    if (child_pids == NULL) {
         perror("malloc failed for child_pids");
-        exit(EXIT_FAILURE);
+        exit(EXIT_MALLOC_FAILED);
     }
 
-    if(!args_handler(&N, msg, &command))
-    {
-        write(STDERR_FILENO, "Error: Invalid arguments.\n", 26);
-        exit(EXIT_FAILURE);
-    }
-
-    for(int i = 0; i < N; i++)
-    {
+    // Fork N children.
+    for (int i = 0; i < N; i++) {
         pid_t pid = fork();
-        if (pid < 0)
-        {
+        if (pid < 0) {
             perror("fork failed");
             free(child_pids);
-            exit(EXIT_FAILURE);
+            exit(EXIT_FORK_FAILED);
         }
-        if (pid == 0)
-        {
-            //child process
-            close(ptc[i][1]); // Close the write end of the pipe
-            close(ctp[i][0]); // Close the read end of the pipe
+        if (pid == 0) {
+            // Child process:
+            close(ptc[i][1]); // Close parent's write end.
+            close(ctp[i][0]); // Close parent's read end.
             int num;
-            while(1)
-            {
-                if (read(ptc[i][0], &num, sizeof(num)) <= 0) break;
-                num *= 2; // Example operation: double the number
+            while (1) {
+                if (read(ptc[i][0], &num, sizeof(num)) <= 0)
+                    break;
+                // Example job: double the number.
+                num *= 2;
                 sleep(5);
-                if (write(ctp[i][1], &num, sizeof(num)) <= 0) break;
+                if (write(ctp[i][1], &num, sizeof(num)) <= 0)
+                    break;
             }
+            exit(0);
         }
-        if (pid > 0)
-        {
+        if (pid > 0) {
             child_pids[i] = pid;
         }
     }
 
-    // Parent process
+    // In the parent process, close the unused ends of the pipes.
     for (int j = 0; j < N; j++) {
-        close(ptc[j][0]); // Parent closes read end of ptc
-        close(ctp[j][1]); // Parent closes write end of ctp
+        close(ptc[j][0]); // Parent writes to ptc.
+        close(ctp[j][1]); // Parent reads from ctp.
     }
 
     char *buffer = (char *)malloc(100 * sizeof(char));
-    if (buffer == NULL)
-    {
+    if (buffer == NULL) {
         perror("malloc failed for buffer");
-        exit(EXIT_FAILURE);
+        exit(EXIT_MALLOC_FAILED);
     }
 
-    int i = 0;
+    int i = 0; // Index for scheduling among children.
 
-    while(1)
-    {   
-        
+    while (1) {
         ssize_t bytes_read = read(STDIN_FILENO, buffer, 100);
-        if (bytes_read <= 0) continue;
+        if (bytes_read <= 0)
+            continue;
         buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline
 
-        if(strcmp(buffer, "help") == 0)
-        {
-            write(STDOUT_FILENO, "Type an integer to send a job to a child of type exit to terminate!\n", 70);
+        if (strcmp(buffer, "help") == 0) {
+            write(STDOUT_FILENO, "Type an integer to send a job to a child or type 'exit' to terminate!\n", 70);
         }
-        if(is_valid_integer(buffer))
-        {   
+        if (is_valid_integer(buffer)) {
             int num = atoi(buffer);
             char assign_msg[100];
             snprintf(assign_msg, sizeof(assign_msg), "Parent Assigned %d to child %d\n", num, child_pids[i]);
             write(STDOUT_FILENO, assign_msg, strlen(assign_msg));
 
-            write(ptc[i][1], &num, sizeof(num)); // Write to the pipe
+            write(ptc[i][1], &num, sizeof(num)); // Send job to the selected child.
 
-            if(command == 1)
-            {
-                i = (i + 1) % N; // Round Robin
-            }
-            else if(command == 2)
-            {
-                i = rand() % N; // Random
-            }
+            // Advance the scheduling index according to the selected mode.
+            if (command == 1)
+                i = (i + 1) % N;
+            else if (command == 2)
+                i = rand() % N;
 
-            // Use select to wait for response from any child
+            // Wait for a response using select().
             fd_set readfds;
             FD_ZERO(&readfds);
             int maxfd = -1;
-
             for (int j = 0; j < N; j++) {
                 FD_SET(ctp[j][0], &readfds);
-                if (ctp[j][0] > maxfd) maxfd = ctp[j][0];
+                if (ctp[j][0] > maxfd)
+                    maxfd = ctp[j][0];
             }
-
             struct timeval timeout;
             timeout.tv_sec = 10;
             timeout.tv_usec = 0;
-
             int ready = select(maxfd + 1, &readfds, NULL, NULL, &timeout);
             if (ready == -1) {
                 perror("select failed");
-            } else if (ready == 0) {
+            }
+            else if (ready == 0) {
                 write(STDOUT_FILENO, "Timeout: No response from children.\n", 37);
-            } else {
+            }
+            else {
                 for (int j = 0; j < N; j++) {
                     if (FD_ISSET(ctp[j][0], &readfds)) {
                         int result;
@@ -187,12 +191,11 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        else if (strcmp(buffer, "exit") == 0)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                kill(child_pids[j], SIGTERM); // Terminate child processes
-                waitpid(child_pids[j], NULL, 0); // Wait for child processes to finish
+        else if (strcmp(buffer, "exit") == 0) {
+            // Terminate all child processes.
+            for (int j = 0; j < N; j++) {
+                kill(child_pids[j], SIGTERM);
+                waitpid(child_pids[j], NULL, 0);
             }
             free(child_pids);
             free(buffer);
@@ -201,7 +204,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // Failsafe cleanup (if somehow loop breaks)
+    // Failsafe cleanup (if loop ever breaks)
     for (int j = 0; j < N; j++) {
         kill(child_pids[j], SIGTERM);
         waitpid(child_pids[j], NULL, 0);
